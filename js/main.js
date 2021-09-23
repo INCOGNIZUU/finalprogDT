2ffcf64aeeea8515305308784e7f72146fee94da
INCOGNIZUU <fajiculayraymondc@gmail.com>
2021-09-23 22:01:44 +0800

    final

---
+++ js/main.js
@@ -0,0 +1,21 @@
+function changePage() {
+  location.href = 'http://127.0.0.1:5500/products.html';
+}
+var x = document.getElementById('p1');
+var y = document.getElementById('p2');
+var z = document.getElementById('p3');
+function pg1() {
+  x.style.display = 'flex';
+  y.style.display = 'none';
+  z.style.display = 'none';
+}
+function pg2() {
+  x.style.display = 'none';
+  y.style.display = 'flex';
+  z.style.display = 'none';
+}
+function pg3() {
+  x.style.display = 'none';
+  y.style.display = 'none';
+  z.style.display = 'flex';
+}
